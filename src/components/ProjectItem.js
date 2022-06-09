3ecdc03bcac35485035f033ad668a7c06d197836
ian <ian.murphy92@gmail.com>
2022-06-09 16:00:55 -0400

    commit tree f14f1dbf5c5ba65e4bdb921c26099360ff0a79e7

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const projectTech = technologies.map((tecnhology, index) => {
+    //console.log(technologies)
+      return <span key={index}>{tecnhology}</span>
+      
+  })
+  //console.log(projectTech)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {projectTech}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
+
+
 function ProjectList({ projects }) {
-  console.log(projects);
+ console.log(projects);
+  const projectView = projects.map((project)=>{
+        return <ProjectItem key = {project.id} technologies = {project.technologies} name = {project.name} about = {project.about}/>
+})
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectView}</div>
+      
     </div>
+    
   );
 }
 
